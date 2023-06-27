42d3bdb1c86cf12ec788dd797ff6766525cb1c27
ligeng <gengli778@gmail.com>
2023-06-28 01:33:46 +0800

    题库页面的创建

---
+++ src/main/resources/static/pages/templateQuestion/index.js
@@ -0,0 +1,283 @@
+let questionList;
+let questionnaireId;
+let questionName;
+let timeLimit;
+
+onload = () => {
+    let id = new URL(location.href).searchParams.get('id');
+
+    let params = {};
+    params.id = id;
+    if (id != null && id !== '') {
+        questionnaireId = id;
+        $.ajax({
+            url: API_BASE_URL + '/queryQuestionnaireList',
+            type: "POST",
+            data: JSON.stringify(params),
+            dataType: "json",
+            contentType: "application/json",
+            success(res) {
+                let questionnaire = res.data[0];
+                timeLimit = questionnaire.answerTimeLimit;
+                questionnaireName = questionnaire.questionnaireName;
+                questionList = questionnaire.questionEntityList;
+                showQuestionnaire(questionList)
+
+            }
+        })
+    } else {
+        questionnaireId = $util.getPageParam('questionnaireId')
+
+        questionList = $util.getPageParam('questionList');
+        showQuestionnaire(questionList)
+    }
+
+}
+
+const showQuestionnaire = (questionList) => {
+    for (let i = 0; i < questionList.length; i++) {
+        if (questionList[i].type == '1') {
+            singleChoiceView(questionList[i], i + 1);
+        } else if (questionList[i].type == '2') {
+            multipleChoiceView(questionList[i], i + 1);
+        } else if (questionList[i].type == '3') {
+            blankView(questionList[i], i + 1);
+        } else if (questionList[i].type == '4') {
+            matrixView(questionList[i], i + 1);
+        } else if (questionList[i].type == '5') {
+            gaugeView(questionList[i], i + 1);
+        } else {
+
+        }
+    }
+}
+
+const singleChoiceView = (question, index) => {
+    $('#problem').append(`
+    <div class="question" id="question${index}" data-type="1" data-problemIndex="${index}">
+      <div class="top">
+        <span class="question-title" id="questionTitle">${index}.${question.problemName}</span>
+        <span class="must-answer" id="mustAnswer"></span>
+      </div>
+      <div class="bottom"></div>
+    </div>
+  `)
+    if (question.mustAnswer) {
+        $('#question' + index + " #mustAnswer").text('必答题')
+    } else {
+        $('#question' + index + " #mustAnswer").text('非必答题')
+    }
+    for (let i = 0; i < question.option.length; i++) {
+        let option = question.option[i];
+        $('#question' + index + " .bottom ").append(`
+        <div style="display: flex; align-items: center; margin-bottom: 3px;">
+          <label class="radio-inline">
+            <input type="radio" name="choice${index}" value="${i}">${option.chooseTerm ? option.chooseTerm : ''}
+          </label>
+        </div>
+    `)
+    }
+}
+
+const multipleChoiceView = (question, index) => {
+    $('#problem').append(`
+    <div class="question" id="question${index}" data-type="2" data-problemIndex="${index}">
+      <div class="top">
+        <span class="question-title" id="questionTitle">${index}.${question.problemName}</span>
+        <span class="must-answer" id="mustAnswer"></span>
+      </div>
+      <div class="bottom"></div>
+    </div>
+  `)
+    if (question.mustAnswer) {
+        $('#question' + index + " #mustAnswer").text('必答题')
+    } else {
+        $('#question' + index + " #mustAnswer").text('非必答题')
+    }
+    for (let i = 0; i < question.option.length; i++) {
+        let option = question.option[i];
+        $('#question' + index + " .bottom ").append(`
+        <div style="display: flex; align-items: center; margin-bottom: 3px;">
+          <label class="checkbox-inline">
+            <input type="checkbox" name="chooseTerm${option.order}" value="${i}">${option.chooseTerm ? option.chooseTerm : ''}
+          </label>
+        </div>
+    `)
+    }
+}
+
+const blankView = (question, index) => {
+    $('#problem').append(`
+    <div class="question" id="question${index}" data-type="3" data-problemIndex="${index}">
+      <div class="top">
+        <span class="question-title" id="questionTitle">${index}.${question.problemName}</span>
+        <span class="must-answer" id="mustAnswer"></span>
+      </div>
+      <div class="bottom"></div>
+    </div>
+  `)
+    if (question.mustAnswer) {
+        $('#question' + index + " #mustAnswer").text('必答题')
+    } else {
+        $('#question' + index + " #mustAnswer").text('非必答题')
+    }
+    $('#question' + index + " .bottom ").append(`
+     <div style="border: 1px solid #CCCCCC; width: 50%; height: 70px;">
+          <textarea class="form-control" id="blankContent" placeholder="请输入答案" rows="4"></textarea>
+      </div>
+  `)
+}
+
+const matrixView = (question, problemIndex) => {
+    $('#problem').append(`
+    <div class="question" id="question${problemIndex}" data-type="4" data-problemIndex="${problemIndex}">
+      <div class="top">
+        <span class="question-title" id="questionTitle">${problemIndex}.${question.problemName}</span>
+        <span class="must-answer" id="mustAnswer"></span>
+      </div>
+      <div class="bottom">
+        <table class="table">
+          <thead>
+            <tr>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            
+          </tbody>
+        </table>
+      </div>
+    </div>
+  `)
+    if (question.mustAnswer) {
+        $('#question' + problemIndex + " #mustAnswer").text('必答题')
+    } else {
+        $('#question' + problemIndex + " #mustAnswer").text('非必答题')
+    }
+    let trs = question.leftTitle ? question.leftTitle.split(',') : [];
+    trs.map((item, index) => {
+        $(`#question${problemIndex} .bottom tbody`).append(`
+      <tr class="tr${index}">
+        <td>${item}</td>
+      </tr>
+    `)
+        question.option.map((item, radioIndex) => {
+            $(`#question${problemIndex} .bottom tbody .tr${index}`).append(`
+        <td>
+          <input type="radio" name="matrx${problemIndex}radio${index}" value="${radioIndex}">
+        </td>
+      `)
+        })
+    })
+    question.option.map(item => {
+        $(`#question${problemIndex} .bottom thead tr`).append(`
+      <th>${item.chooseTerm}</th>
+    `)
+    })
+}
+
+const gaugeView = (question, problemIndex) => {
+    $('#problem').append(`
+    <div class="question" id="question${problemIndex}" data-type="5" data-problemIndex="${problemIndex}">
+      <div class="top">
+        <span class="question-title" id="questionTitle">${problemIndex}.${question.problemName}</span>
+        <span class="must-answer" id="mustAnswer"></span>
+      </div>
+      <div class="bottom" style="display: flex; align-items: center; justify-content: space-between;">
+      </div>
+    </div>
+  `)
+    if (question.mustAnswer) {
+        $('#question' + problemIndex + " #mustAnswer").text('必答题')
+    } else {
+        $('#question' + problemIndex + " #mustAnswer").text('非必答题')
+    }
+    $(`#question${problemIndex} .bottom`).append(`
+    <div>${question.option[0].chooseTerm}</div>
+  `)
+    question.option.map((item, index) => {
+        $(`#question${problemIndex} .bottom`).append(`
+      <div>
+        <label class="radio-inline">
+          <input type="radio" name="fraction${problemIndex}" value="${index}"/>${item.fraction}
+        </label>
+      </div>
+    `)
+    })
+    $(`#question${problemIndex} .bottom`).append(`
+    <div>${question.option[question.option.length - 1].chooseTerm}</div>
+  `)
+}
+
+const onSubmitQuestionnaire = () => {
+
+    if (!answerLimitCheck()){
+        return;
+    }
+
+
+
+    let formData = collectFormData();
+    if(formData == null)
+    {
+        return;
+    }
+    let record = {};
+    record.answeredBy = formData.answererName;
+    record.questionnaireId = questionnaireId;
+    let answerList = [];
+    for (let i = 0; i < formData.answers.length; i++) {
+        let input = formData.answers[i];
+        if (input.answerType == '1') {
+            let answer = {};
+            answer.type = input.answerType;
+            answer.questionIndex = input.problemIndex;
+            answer.chooseTerm = input.selectedOption;
+            answerList.push(answer);
+        } else if (input.answerType == '2') {
+            for (let i = 0; i < input.selectedOptions.length; i++) {
+                let answer = {};
+                answer.type = input.answerType;
+                answer.questionIndex = input.problemIndex;
+                answer.chooseTerm = input.selectedOptions[i];
+                answerList.push(answer);
+            }
+        } else if (input.answerType == '3') {
+            let answer = {};
+            answer.type = input.answerType;
+            answer.questionIndex = input.problemIndex;
+            answer.chooseTerm = input.inputValue;
+            answerList.push(answer);
+        } else if (input.answerType == '4') {
+            for (let i = 0; i < input.selectedOptions.length; i++) {
+                let answer = {};
+                answer.type = input.answerType;
+                answer.questionIndex = input.problemIndex;
+                answer.chooseTerm = input.selectedOptions[i];
+                answer.row = i;
+                answerList.push(answer);
+            }
+        } else if (input.answerType == '5') {
+            let answer = {};
+            answer.type = input.answerType;
+            answer.questionIndex = input.problemIndex;
+            answer.chooseTerm = input.selectedOption;
+            answerList.push(answer);
+        } else {
+
+        }
+    }
+
+    record.answerEntityList = answerList;
+
+    $.ajax({
+        url: API_BASE_URL + '/addRecord',
+        type: "POST",
+        data: JSON.stringify(record),
+        dataType: "json",
+        contentType: "application/json",
+        success(res) {
+            alert('提交成功!')
+        }
+    })
+}
